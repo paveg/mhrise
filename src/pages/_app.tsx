026c3116488ce236efc9e90b0cbc6aa3a582b540
Ryota Ikezawa <pavegy@gmail.com>
2021-05-05 17:36:24 +0900

    Preparation config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import { AppProps } from 'next/app';
+import reset from 'styled-reset';
+import React from 'react';
+import Head from 'next/head';
+import {
+    ThemeProvider as StyledComponentsThemeProvider,
+    createGlobalStyle,
+} from 'styled-components';
+import { ThemeProvider as MaterialUIThemeProvider, StylesProvider } from '@material-ui/styles';
+
+import theme from '../styles/theme';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  // Write your global styles.
+`;
+
+const App = ({ Component, pageProps }: AppProps) => {
+    React.useEffect(() => {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles && jssStyles.parentNode) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <meta
+                    key="viewport"
+                    name="viewport"
+                    content="width=device-width,initial-scale=1.0,viewport-fit=cover"
+                />
+                <meta name="Description" content="sample cms with next.js" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
+                    rel="preload"
+                    as="style"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c"
+                    rel="preload"
+                    as="style"
+                />
+                <title key="title">Sample CMS</title>
+            </Head>
+
+            <StylesProvider injectFirst>
+                <MaterialUIThemeProvider theme={theme}>
+                    <StyledComponentsThemeProvider theme={theme}>
+                        <GlobalStyle />
+                        <Component {...pageProps} />
+                    </StyledComponentsThemeProvider>
+                </MaterialUIThemeProvider>
+            </StylesProvider>
+        </>
+    );
+};
+
+export default App;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,50 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets as MaterialServerStyleSheets } from '@material-ui/core';
+import * as React from 'react';
+
+class MyDocument extends Document {
+  render() {
+    return (
+        <Html lang="ja-JP">
+          <Head>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <body>
+          <Main />
+          <NextScript />
+          </body>
+        </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const scSheet = new ServerStyleSheet();
+  const muiSheets = new MaterialServerStyleSheets();
+  const origRenderPage = ctx.renderPage;
+
+  const renderOptions = {
+    enhanceApp: (App) => (props) => scSheet.collectStyles(muiSheets.collect(<App {...props} />)),
+  };
+
+  try {
+    ctx.renderPage = () => origRenderPage(renderOptions);
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        <>
+          {initialProps.styles}
+          {scSheet.getStyleElement()}
+          {muiSheets.getStyleElement()}
+        </>,
+      ],
+    };
+  } finally {
+    scSheet.seal();
+  }
+};
+
+export default MyDocument;
